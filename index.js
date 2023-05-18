1927a7e5010ff08b740e6587ef11ef578825fcfb
Abir <abirovi481@gmail.com>
2023-05-18 16:00:14 +0600

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -7,6 +9,30 @@ const port = process.env.PORT || 5000
 // middleware 
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.maovuz9.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    // await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  finally {
+    // await client.close();
+  }
+}
+run().catch(console.log);
 
 app.get('/', (req, res) => {
     res.send('Toy Land is running')
